1a3599def3102be08c5037562e8ba0f5386ffc77
junieguo <junieguo@sas.upenn.edu>
2025-11-06 22:25:36 -0500

    step 2

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,3 +10,26 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+# Get current AWS account ID
+data "aws_caller_identity" "current" {}
+
+# Create ECR repository for our Docker image
+resource "aws_ecr_repository" "webapp" {
+  name                 = "terraform-webapp-${var.pennkey}"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name = "terraform-webapp-${var.pennkey}"
+  }
+}
+
+# Output the ECR repository URL
+output "ecr_repository_url" {
+  description = "ECR repository URL for Docker images"
+  value       = aws_ecr_repository.webapp.repository_url
+}
